f6a6eb1b23982f573b873f447e5bbf2560cf67b0
Michele <mikelerogeratro@yahoo.it>
2018-10-15 18:23:56 +0200

    Tweaks

--- ScrapEngine/ScrapEngine/Engine/Rendering/Texture/TextureImage.cpp
+++ ScrapEngine/ScrapEngine/Engine/Rendering/Texture/TextureImage.cpp
@@ -29,7 +29,6 @@ ScrapEngine::TextureImage::TextureImage(std::string file_path, vk::Device* input
 
 	transitionImageLayout(&textureImage, vk::Format::eR8G8B8A8Unorm, vk::ImageLayout::eUndefined, vk::ImageLayout::eTransferDstOptimal);
 	StagingBuffer::copyBufferToImage(deviceRef ,StaginfBufferRef->getStagingBuffer(), &textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight), CommandPool, graphicsQueue);
-	//transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
 	delete StaginfBufferRef;
 
