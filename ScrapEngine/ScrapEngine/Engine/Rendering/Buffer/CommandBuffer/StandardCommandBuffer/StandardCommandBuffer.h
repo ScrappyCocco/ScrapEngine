8ef83d1eb57ee8f94c70d49acb28786ddeae9dc9
Michele <mikelerogeratro@yahoo.it>
2019-07-27 12:06:47 +0200

    Standard and gui command buffers

--- ScrapEngine/ScrapEngine/Engine/Rendering/Buffer/CommandBuffer/StandardCommandBuffer/StandardCommandBuffer.h
+++ ScrapEngine/ScrapEngine/Engine/Rendering/Buffer/CommandBuffer/StandardCommandBuffer/StandardCommandBuffer.h
@@ -1,28 +1,23 @@
 #pragma once
 
-#include <vulkan/vulkan.hpp>
-#include <vector>
+#include <Engine/Rendering/Buffer/CommandBuffer/BaseCommandBuffer.h>
 #include <Engine/Rendering/Buffer/FrameBuffer/VulkanFrameBuffer.h>
 #include <Engine/Rendering/Model/SkyboxInstance/VulkanSkyboxInstance.h>
 #include <Engine/Rendering/Model/MeshInstance/VulkanMeshInstance.h>
 #include <Engine/Rendering/CommandPool/VulkanCommandPool.h>
-#include <Engine/Rendering/Gui/VulkanImGui.h>
 
 namespace ScrapEngine
 {
 	namespace Render
 	{
-		class VulkanCommandBuffer
+		class StandardCommandBuffer : public BaseCommandBuffer
 		{
 		private:
-			std::vector<vk::CommandBuffer> command_buffers_;
-			vk::RenderPassBeginInfo render_pass_info_;
-			VulkanCommandPool* command_pool_ref_;
 			Camera* current_camera_;
 		public:
-			VulkanCommandBuffer() = default;
+			StandardCommandBuffer() = default;
 
-			~VulkanCommandBuffer();
+			~StandardCommandBuffer() = default;
 
 			void init_command_buffer(VulkanFrameBuffer* swap_chain_frame_buffer,
 			                         vk::Extent2D* input_swap_chain_extent_ref,
@@ -31,13 +26,6 @@ namespace ScrapEngine
 
 			void load_skybox(VulkanSkyboxInstance* skybox_ref);
 			void load_mesh(const VulkanMeshInstance* mesh);
-			void load_ui(VulkanImGui* gui, short int index);
-
-			void close_command_buffer();
-
-			void free_command_buffers();
-
-			const std::vector<vk::CommandBuffer>* get_command_buffers_vector() const;
 		};
 	}
 }
