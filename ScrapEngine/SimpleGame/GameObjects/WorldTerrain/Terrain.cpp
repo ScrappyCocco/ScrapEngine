579e6a18f341b59c24d380d827515a9b07121f15
Michele <mikelerogeratro@yahoo.it>
2019-07-30 09:24:24 +0200

    All terrains

--- ScrapEngine/SimpleGame/GameObjects/WorldTerrain/Terrain.cpp
+++ ScrapEngine/SimpleGame/GameObjects/WorldTerrain/Terrain.cpp
@@ -3,18 +3,33 @@
 
 Terrain::Terrain(ScrapEngine::Core::ComponentsManager* input_component_manager,
                  const ScrapEngine::Core::SVector3& position,
-                 const ScrapEngine::Core::SVector3& scale)
+                 const ScrapEngine::Core::SVector3& scale,
+                 const color c)
 	: SGameObject("Terrain game object")
 {
 	set_object_location(position);
 	set_object_scale(scale);
+
+	std::string color_texture = "../assets/textures/SimpleWhiteTexture.png";
+	switch (c)
+	{
+	case green:
+		color_texture = "../assets/textures/SimpleGreenTexture.png";
+		break;
+	case red:
+		color_texture = "../assets/textures/SimpleRedTexture.png";
+		break;
+	case finish:
+		color_texture = "../assets/textures/Finish.png";
+		break;
+	}
 
 	//Mesh
 	ScrapEngine::Core::MeshComponent* mesh = input_component_manager->create_new_mesh_component(
 		"../assets/shader/compiled_shaders/shader_base.vert.spv",
 		"../assets/shader/compiled_shaders/shader_base.frag.spv",
 		"../assets/models/cube.obj",
-		{"../assets/textures/SimpleWhiteTexture.png"}
+		{color_texture}
 	);
 	add_component(mesh);
 
--- ScrapEngine/SimpleGame/GameObjects/WorldTerrain/Terrain.h
+++ ScrapEngine/SimpleGame/GameObjects/WorldTerrain/Terrain.h
@@ -6,9 +6,17 @@
 class Terrain : public ScrapEngine::Core::SGameObject
 {
 public:
+	enum color
+	{
+		red,
+		white,
+		green,
+		finish
+	};
+
 	Terrain(ScrapEngine::Core::ComponentsManager* input_component_manager,
 	        const ScrapEngine::Core::SVector3& position,
-	        const ScrapEngine::Core::SVector3& scale);
+	        const ScrapEngine::Core::SVector3& scale,
+	        color c = white);
 	~Terrain() = default;
 };
-
