a806451ae6cffd3fc7f0c9ef6371f42ee58bbebb
Michele <mikelerogeratro@yahoo.it>
2019-10-01 18:54:10 +0200

    Added main menu button hover to display cursor changing

--- ScrapEngine/SimpleGame/GameObjects/MainMenu/MainMenu.h
+++ ScrapEngine/SimpleGame/GameObjects/MainMenu/MainMenu.h
@@ -11,13 +11,20 @@ private:
 	ScrapEngine::Core::LogicManagerView* logic_view_ref_ = nullptr;
 	Ball* player_ref_ = nullptr;
 	ScrapEngine::Render::GameWindow* window_ref_ = nullptr;
+	ScrapEngine::Input::InputManager* input_ref_ = nullptr;
 
 	bool game_started_ = false;
+	bool play_button_cursor_changed_ = false;
+	bool exit_button_cursor_changed_ = false;
 public:
 	MainMenu(ScrapEngine::Core::LogicManagerView* logic_view,
 	         Ball* player,
-	         ScrapEngine::Render::GameWindow* window_ref);
+	         ScrapEngine::Render::GameWindow* window_ref,
+	         ScrapEngine::Input::InputManager* input_ref);
 	~MainMenu() = default;
-
+private:
 	void on_gui() override;
+
+	void set_hand_cursor() const;
+	void set_arrow_cursor() const;
 };
